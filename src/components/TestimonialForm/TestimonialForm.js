10f935f96fdfc6f2e93697257687bee83bb10f4a
Napphy <dosgavinojr@mymail.mapua.edu.ph>
2024-05-07 10:26:46 +0800

    upload testimonial

--- src/components/TestimonialForm/TestimonialForm.js
+++ src/components/TestimonialForm/TestimonialForm.js
@@ -1,43 +1,75 @@
 import React from 'react';
 import './TestimonialForm.scss';
 import icon from '../../assets/banner/icons/Calling.png';
+import axios from 'axios';
 
 const ContactForm = () => {
+
+    const handleSubmit = async (e) => {
+    e.preventDefault(); // Prevent default form submission behavior
+
+    try {
+        const name = e.target.name.value;
+        const applicantType = e.target.applicantType.value;
+        const email = e.target.email.value;
+        const contactNumber = e.target.contactNumber.value;
+        const text = e.target.text.value;
+
+        const testimonialData = {
+        name,
+        applicantType,
+        email,
+        contactNumber,
+        text,
+        };
+
+        const response = await axios.post('https://lks-server.onrender.com/uploadTestimonial', testimonialData);
+        if (response.status === 201) { 
+        console.log('Testimonial submitted successfully!');
+    
+        } else {
+        console.error('Error submitting testimonial:', response.data);
+        }
+    } catch (error) {
+        console.error('Error submitting testimonial:', error);
+    }
+    }
+
     return (
-        <form>
+        <form onSubmit={handleSubmit}>
             <div className="row">
                 <div className="col-lg-6">
                     <div class="form-group">
                         <label>Name</label>
-                        <input type="email" class="form-control" placeholder="Enter your name" />
+                        <input type="text" name='name' class="form-control" placeholder="Enter your name" />
                     </div>
                 </div>
                 <div className="col-lg-6">
                     <div class="form-group">
                         <label>I am a/an</label>
-                        <select class="form-control">
-                            <option>Alumni</option>
-                            <option>Parent</option>
+                        <select name='applicantType' class="form-control">
+                            <option value='Alum'>Alumni</option>
+                            <option value='Parent'>Parent</option>
                         </select>
                     </div>
                 </div>
                 <div className="col-lg-6">
                     <div class="form-group">
                         <label>E-mail Address</label>
-                        <input type="email" class="form-control" placeholder="Enter email address" />
+                        <input type="email" name='email' class="form-control" placeholder="Enter email address" />
                     </div>
                 </div>
                 <div className="col-lg-6">
                     <div class="form-group">
                         <label>Contact Number</label>
-                        <input type="email" class="form-control" placeholder="Enter phone number" />
+                        <input type="text" name='contactNumber' class="form-control" placeholder="Enter phone number" />
                     </div>
                 </div>
                 
                 <div className="col-lg-12">
                     <div class="form-group">
                         <label for="exampleFormControlTextarea1">Your Testinomial</label>
-                        <textarea class="form-control" placeholder='Enter your story' rows="3"></textarea>
+                        <textarea class="form-control" name='text' placeholder='Enter your story' rows="3"></textarea>
                     </div>
                 </div>
 
